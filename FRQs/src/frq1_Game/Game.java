7fa12a395dc75044af179439ca6d4c7725578bbc
s-sseshadri <s-sseshadri@484M121394.lwsd.k12wa.org>
2022-05-11 12:11:22 -0700

    first frq game class fixes

--- FRQs/src/frq1_Game/Game.java
+++ FRQs/src/frq1_Game/Game.java
@@ -41,19 +41,19 @@ public class Game {
         int score = 0;
         boolean done = levelOne.goalReached();
 
-        if (!done) {
+        if (done) {
             score += levelOne.getPoints();
             done = levelTwo.goalReached();
         }
-        if (!done) {
+        if (done) {
             score += levelTwo.getPoints();
             done = levelThree.goalReached();
         }
-        if (!done) {
+        if (done) {
             score += levelThree.getPoints();
             done = isBonus();
         }
-        if (!done) {
+        if (done) {
             score *= 3;
         }
         
@@ -67,7 +67,7 @@ public class Game {
     public int playManyTimes(int num) {
         
         /* IMPLEMENTATION OF PART (B) */
-        int max = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
         for (int i = 0; i < num; i++) {
             play();
             max = Math.max(max, getScore());
