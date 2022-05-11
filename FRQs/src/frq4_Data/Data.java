9e398ae8d9a1814a99e3fdac4be7e14a5fd83f11
Florin.Teo <Florin.Teo@live.com>
2022-05-11 09:28:13 -0700

    Reset Data to buggy version

--- FRQs/src/frq4_Data/Data.java
+++ FRQs/src/frq4_Data/Data.java
@@ -1,7 +1,5 @@
 package frq4_Data;
 
-import java.util.ArrayList;
-
 /**
  * Class Data is part of Question 4 from the FRQs in the
  * 2022 AP Computer Science A exam.
@@ -10,7 +8,7 @@ import java.util.ArrayList;
  */
 public class Data {
 
-    public static final int MAX = 1000; /* value provided as example */
+    public static final int MAX = 13; /* value provided as example */
     private int[][] grid;
     
     /** Fills all elements of grid with randomly generated values,
@@ -19,26 +17,26 @@ public class Data {
      *     grid has at least one element.
      */
     public void repopulate() {
-        /* to be implemented in part (a) */
         
-        // given MAX, we have as many choices as multiple of 10s are in the range [1..MAX]
-        // from which we need to exclude the multiple of 100.
-        int nChoices = MAX/10 - MAX/100;
-        
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                // pick a random choice in the range [1..nChoices]
-                int choice = (int)(1 + nChoices * Math.random());
-                // Generate the random number verifying all constraints:
-                //
-                // choice:            1  2 ...  9  10  11 ...  18  19 ...  27  28 ...  
-                // choice*10:        10 20 ... 90 100 110 ... 180 190 ... 270 280 ...
-                // offset:            0  0 ...  0  10  10 ...  10  20 ...  20  30 ...
-                // choice*10+offset: 10 20 ... 90 110 120 ... 190 210 ... 290 310 ...
-                //
-                // Offset starts from 0 and with every group of 9 choices it grows by 10 
-                // => offset = 10 * (choice-1) / 9
-                grid[i][j] = 10 * choice + 10 * (choice - 1) / 9;
+        /* TO BE IMPLEMENTED IN PART (A) */
+        for (int r = 0; r < grid[0].length; r++) {
+            for (int c = 0; c < grid.length; c++) {
+                int value = 0;
+                
+                // if MAX is less than 10 then there's no chance for
+                // any random value to meet problem requirements and
+                // the while() loop will spin forever. Guard against
+                // that by setting initial value to something that
+                // will prevent entering the loop all together.
+                if (MAX < 10) {
+                    value = 10;
+                }
+                
+                while (value % 10 == 0 && value % 100 != 0) {
+                    value = (int) (Math.random() * MAX) + 1;
+                }
+                
+                grid[r][c] = value;
             }
         }
     }
@@ -49,13 +47,14 @@ public class Data {
      *    grid has at least one element.
      */
     public int countIncreasingCols() {
-        /* to be implemented in part (b) */
+        
+        /* TO BE IMPLEMENTED IN PART (B) */
         int count = 0;
         
         for (int c = 0; c < grid[0].length; c++) {
-            boolean increasing = true;
+            boolean increasing = false;
             for (int r = 1; increasing && r < grid.length; r++) {
-                increasing = (grid[r-1][c] <= grid[r][c]);
+                increasing = (grid[r-1][c] > grid[r][c]);
             }
             if (increasing) {
                 count++;
@@ -66,5 +65,4 @@ public class Data {
     }
     
     // There may be instance variables, constructors, and methods that are not shown.
-
 }
